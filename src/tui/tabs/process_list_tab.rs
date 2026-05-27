e8391c60fc57a72cd2a081f2201a0b1c6d2e3b4d
SallimanR <sallimanr888@gmail.com>
2026-05-27 18:04:53 +0000

    Remove: unnecessary comment

--- src/tui/tabs/process_list_tab.rs
+++ src/tui/tabs/process_list_tab.rs
@@ -6,8 +6,6 @@ use crate::{
 };
 
 pub fn render_process_list_tab(app: &mut App, frame: &mut Frame, area: Rect) {
-    // app.panes.process_list_pane.render(frame, area);
-    // app.panes.process_tree_pane.render(frame, area);
     match &mut app.panes.processes {
         ProcessListOrTree::List(processes) => {
             processes.render(frame, area);
